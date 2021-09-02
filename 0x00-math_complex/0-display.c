a91031df6e3ac14cd9e0451f81ee5cf03e6e453c
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-01 22:12:32 -0300

    fixed betty

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -8,39 +8,31 @@
 * Return: Always 0 (Success)
 */
 
-#include "holberton.h"
-
-/**
- * display_complex_number - displays a complex numbers
- * @c: complex umber to display.
- */
-
 void display_complex_number(complex c)
 {
 
-/* print the real part */
 if (c.re)
-    printf("%.9g", c.re);
+	printf("%.9g", c.re);
 
-/* print the imaginary part */
 if (c.im > 0)
 {
-    if (c.re)
-        printf(" + ");
-    if (c.im != 1)
-        printf("%.9gi\n", c.im);
-    else
-        printf("i\n");
+	if (c.re)
+		printf(" + ");
+	if (c.im != 1)
+		printf("%.9gi\n", c.im);
+	else
+		printf("i\n");
 }
-else if (c.im < 0)
-{
-    if (c.re)
-        printf(" - ");
-    if (c.im != -1)
-        printf("%.9gi\n", c.im * (-1));
-    else
-        printf("i\n");
-}
-else
-    printf("\n");
+else 
+	if (c.im < 0)
+	{
+		if (c.re)
+			printf(" - ");
+		if (c.im != -1)
+			printf("%.9gi\n", c.im * (-1));
+		else
+			printf("i\n");
+	}
+	else
+		printf("\n");
 }
